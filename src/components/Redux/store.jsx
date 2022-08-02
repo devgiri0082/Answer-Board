dfbe24a69a83540d5c57a6351aac61e38081accd
Dev Giri <devgiri0082@gmail.com>
2022-08-02 19:55:15 +0545

    Delete src/components directory

--- src/components/Redux/store.jsx
+++
@@ -1,7 +0,0 @@
-import { applyMiddleware, createStore } from "redux";
-import { createLogger } from "redux-logger";
-import thunk from "redux-thunk";
-import todoReducer from "./reducers/todoReducer"
-
-let myLogger = createLogger();
-export const store = createStore(todoReducer, applyMiddleware(myLogger, thunk));
